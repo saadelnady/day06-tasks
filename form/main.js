a84d1cb149c725b8d12af65faa86468426f297ea
Saad Elnady <saad.elnady.work@gmail.com>
2023-07-29 19:50:14 +0300

    change form

--- form/main.js
+++ form/main.js
@@ -1,47 +1,50 @@
-var userName = document.getElementById("userName");
-var userAge = document.getElementById("userAge");
-var userGenders = document.querySelectorAll("#userGender input[type='radio']");
-var favColor = document.querySelector("#favColor");
-var submit = document.getElementById("submit");
+/** @format */
+
+var userName = document.getElementById('userName');
+var userAge = document.getElementById('userAge');
+var userGender = document.querySelectorAll("#userGender input[type='radio']");
+var favColor = document.querySelector('#favColor');
+var submit = document.getElementById('submit');
 
 submit.onclick = function (e) {
-  e.preventDefault();
-  var gender;
-  for (var i = 0; i < userGenders.length; i++) {
-    if (userGenders[i].checked) {
-      gender = userGenders[i].value;
-    }
-  }
+	e.preventDefault();
+	var gender;
+	for (var i = 0; i < userGender.length; i++) {
+		if (userGender[i].checked) {
+			gender = userGender[i].value;
+			break;
+		}
+	}
 
-  var user = {
-    userVisiting: 0,
-    userNameValue: userName.value,
-    userAgeValue: userAge.value,
-    userGenderValue: gender,
-    favColorValue: favColor.value,
-  };
-  const expires = new Date();
-  expires.setFullYear(expires.getFullYear() + 1);
+	var user = {
+		userName: userName.value,
+		userAge: userAge.value,
+		userGender: gender,
+		favColor: favColor.value,
+	};
 
-  document.cookie = `userVisiting=${
-    user.userVisiting
-  }; expires=${expires.toUTCString()}`;
+	var userVisiting = (parseInt(getCookie('userVisiting')) || 0) + 1;
 
-  document.cookie = `userNameValue=${
-    user.userNameValue
-  }; expires=${expires.toUTCString()}`;
+	setCookie('userName', user.userName);
+	setCookie('userVisiting', userVisiting);
+	setCookie('userAge', user.userAge);
+	setCookie('favColor', user.favColor);
+	setCookie('userGender', user.userGender);
 
-  document.cookie = `userAgeValue=${
-    user.userAgeValue
-  }; expires=${expires.toUTCString()}`;
-
-  document.cookie = `userGenderValue=${
-    user.userGenderValue
-  }; expires=${expires.toUTCString()}`;
-
-  document.cookie = `favColorValue=${
-    user.favColorValue
-  }; expires=${expires.toUTCString()}`;
-
-  window.open("./wellcome.html", "_self");
+	window.open('./wellcome.html', '_self');
 };
+function getCookie(name) {
+	var cookies = document.cookie.split(';').map((cookie) => cookie.trim());
+	console.log('cookies', cookies);
+	for (var i = 0; i < cookies.length; i++) {
+		var cookie = cookies[i].split('=');
+		if (cookie[0] === name) {
+			return cookie[1];
+		}
+	}
+	return null;
+}
+
+function setCookie(name, value) {
+	document.cookie = name + '=' + value + ';';
+}
--- form/main2.js
+++ form/main2.js
@@ -1,56 +1,41 @@
-var wellcomeDiv = document.getElementById("wellcomeDiv");
-var userParagraph = document.getElementById("userParagraph");
-var timesOfViste = document.querySelector("#wellcomeDiv p .times");
-function wellcomeUser() {
-  var user = getUserObjectFromCookies();
+/** @format */
 
-  if (user.userGenderValue == "male") {
-    userImage.src = "./images/male.jpg";
-  } else {
-    userImage.src = "./images/female.jpg";
-  }
+window.onload = function () {
+	welcomeUser();
+};
 
-  userParagraph.innerHTML = user.userNameValue;
-  userParagraph.style.color = user.favColorValue;
+function welcomeUser() {
+	var user = {
+		visiting: parseInt(getCookie('userVisiting')) || 0,
+		userName: getCookie('userName'),
+		userAge: getCookie('userAge'),
+		favColor: getCookie('favColor'),
+		userGender: getCookie('userGender'),
+	};
+	console.log(user);
+	var userImage = document.getElementById('userImage');
+	userImage.src =
+		user.userGender === 'male' ? './images/male.jpg' : './images/female.jpg';
 
- 
+	var userParagraph = document.getElementById('userParagraph');
+	userParagraph.innerHTML = user.userName;
+	userParagraph.style.color = user.favColor;
+
+	var timesOfVisit = document.querySelector('#wellcomeDiv p .times');
+	timesOfVisit.innerHTML = user.visiting + 1;
 }
-wellcomeUser();
 
- 
-function getUserObjectFromCookies() {
-  const cookies = document.cookie.split("; ");
-  let user = {
-    userVisiting: 0,
-    userNameValue: "",
-    userAgeValue: "",
-    userGenderValue: "",
-    favColorValue: "",
-  };
+function getCookie(name) {
+	var cookies = document.cookie.split(';').map((cookie) => cookie.trim());
+	for (var i = 0; i < cookies.length; i++) {
+		var cookie = cookies[i].split('=');
+		if (cookie[0] === name) {
+			return cookie[1];
+		}
+	}
+	return null;
+}
 
-  cookies.forEach((cookie) => {
-    const [name, value] = cookie.split("=");
-
-    switch (name) {
-      case "userVisiting":
-        user.userVisiting = parseInt(value);
-        break;
-      case "userNameValue":
-        user.userNameValue = value;
-        break;
-      case "userAgeValue":
-        user.userAgeValue = value;
-        break;
-      case "userGenderValue":
-        user.userGenderValue = value;
-        break;
-      case "favColorValue":
-        user.favColorValue = value;
-        break;
-      default:
-        break;
-    }
-  });
-
-  return user;
+function setCookie(name, value) {
+	document.cookie = name + '=' + value + ';';
 }
